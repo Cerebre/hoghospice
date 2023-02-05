171310d0bfc79ad7220a972b2bde186e5ccbe4b7
McFrase <51033165+McFrase@users.noreply.github.com>
2023-02-05 17:50:45 +0100

    Donation main page was added alongside some responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
 // Country Code Input
-document.addEventListener('DOMContentLoaded', function () {
-  var input = document.querySelector('.country-code-input');
-  var iti = window.intlTelInput(input, {
-    preferredCountries: ['ng'],
-    separateDialCode: true,
-    nationalMode: false,
+if (document) {
+  document.addEventListener('DOMContentLoaded', function () {
+    var input = document.querySelector('.country-code-input');
+    var iti = window.intlTelInput(input, {
+      preferredCountries: ['ng'],
+      separateDialCode: true,
+      nationalMode: false,
+    });
   });
-});
+}
 
 // Play and Popup Video
 function playStory() {
@@ -19,78 +21,104 @@ function playStory() {
 
 // Card Testimonial Code
 const buttons = document.querySelectorAll('.custom-card');
-const slide = document.querySelectorAll('.story-container');
+if (buttons) {
+  const slide = document.querySelectorAll('.story-container');
+  let activeBtn = buttons[0];
 
-let activeBtn = buttons[0];
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    activeBtn.classList.remove('active');
-    activeBtn = button;
-    activeBtn.classList.add('active');
-    const activeContent = document.getElementById(`${button.id}-content`);
-    slide.forEach((content) => {
-      content.classList.remove('active');
-      gsap.to(content, 0.8, {
-        opacity: 0,
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      activeBtn.classList.remove('active');
+      activeBtn = button;
+      activeBtn.classList.add('active');
+      const activeContent = document.getElementById(`${button.id}-content`);
+      slide.forEach((content) => {
+        content.classList.remove('active');
+        gsap.to(content, 0.8, {
+          opacity: 0,
+        });
+      });
+      activeContent.classList.add('active');
+      gsap.to(activeContent, 0.8, {
+        opacity: 1,
       });
     });
-    activeContent.classList.add('active');
-    gsap.to(activeContent, 0.8, {
-      opacity: 1,
-    });
   });
-});
+}
 
+// Hamburger Controllers
 const barControl = document.querySelector('.bars');
 const bodyElement = document.querySelector('body');
+if (barControl) {
+  barControl.addEventListener('click', function () {
+    this.classList.toggle('active');
+    if (this.classList.contains('active')) {
+      gsap.to('.bar-one', {
+        rotate: '45deg',
+        top: '50%',
+        y: '-50%',
+        transformOrigin: 'center',
+      });
+      gsap.to('.bar-three', {
+        rotate: '-45deg',
+        top: '50%',
+        y: '-50%',
+        transformOrigin: 'center',
+      });
+      gsap.to('.bar-two', {
+        scale: '0',
+        // opacity: 0,
+      });
+      gsap.to('.menu', {
+        right: '0',
+      });
+      gsap.to('body', {
+        overflow: 'hidden',
+      });
+    } else {
+      gsap.to('.bar-one', {
+        rotate: '0deg',
+        top: '25%',
+        y: '0%',
+        transformOrigin: 'center',
+      });
+      gsap.to('.bar-three', {
+        rotate: '0deg',
+        top: '75%',
+        y: '-75%',
+        transformOrigin: 'center',
+      });
+      gsap.to('.bar-two', {
+        scale: '1',
+      });
+      gsap.to('.menu', {
+        right: '-100%',
+      });
+      gsap.to('body', {
+        overflow: 'scroll',
+      });
+    }
+  });
+}
 
-barControl.addEventListener('click', function () {
-  this.classList.toggle('active');
-  if (this.classList.contains('active')) {
-    gsap.to('.bar-one', {
-      rotate: '45deg',
-      top: '50%',
-      y: '-50%',
-      transformOrigin: 'center',
-    });
-    gsap.to('.bar-three', {
-      rotate: '-45deg',
-      top: '50%',
-      y: '-50%',
-      transformOrigin: 'center',
-    });
-    gsap.to('.bar-two', {
-      scale: '0',
-      // opacity: 0,
-    });
-    gsap.to('.menu', {
-      right: '0',
-    });
-    gsap.to('body', {
-      overflow: 'hidden',
-    });
-  } else {
-    gsap.to('.bar-one', {
-      rotate: '0deg',
-      top: '25%',
-      y: '0%',
-      transformOrigin: 'center',
-    });
-    gsap.to('.bar-three', {
-      rotate: '0deg',
-      top: '75%',
-      y: '-75%',
-      transformOrigin: 'center',
-    });
-    gsap.to('.bar-two', {
-      scale: '1',
-    });
-    gsap.to('.menu', {
-      right: '-100%',
-    });
-    gsap.to('body', {
-      overflow: 'scroll',
-    });
+// Shortening Texts On Mobile
+const mq = window.matchMedia('(max-width: 768px)');
+
+if (mq.matches) {
+  const shortTitle = document.getElementsByClassName('checkTextLenght');
+
+  for (let i = 0; i < shortTitle.length; i++) {
+    let shortTitles = shortTitle[i];
+    if (shortTitles.innerText.length > 46) {
+      shortTitles.innerText = shortTitles.innerText.substring(0, 46) + '...';
+    }
   }
-});
+
+  const shortExerpt = document.getElementsByClassName('excerpt');
+
+  for (let i = 0; i < shortExerpt.length; i++) {
+    let shortExerpts = shortExerpt[i];
+    if (shortExerpts.innerText.length > 127) {
+      shortExerpts.innerText = shortExerpts.innerText.substring(0, 112) + '...';
+    }
+  }
+}
